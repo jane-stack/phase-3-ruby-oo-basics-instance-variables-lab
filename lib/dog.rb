bc7071a1ece0dcb3dc20f18e15a462e9bcffdf22
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-02-08 12:12:36 -0700

    commit tree 779a79b98111d2d94b2aa2dac6d30ff1755a620a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
-
+    def name=(dog_name)
+        # using instance variable to set and get
+        @this_dogs_name = dog_name
+    end
+    def name 
+        @this_dogs_name
+    end
 end
